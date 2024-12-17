8438c9ef4707997ad3b024fff6b39b43a2e754ca
timoshenko vladimir <>
2024-12-18 02:50:31 +0300

    lab4

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const greeting = "Это тестовый проект";
+
+  console.log('This is App ');
+  return (
+    <div className="App">
+     <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+         {greeting}
+      </header>
+    </div>
+  );
+}
+
+export default App;
