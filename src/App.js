238e6b736210cddce0a68a55b245766ec3294875
timoshenko vladimir <>
2024-12-18 04:29:10 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   const greeting = "Это тестовый проект";
+  const greeting_1 = "Это тестовый проект";
+  const greeting_2 = "Это тестовый проект";
 
   return (
     <div className="App">
